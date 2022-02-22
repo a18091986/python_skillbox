905118032c1394bc04a8996c9d70d742d9fd0ed5
behappyman <behappyman@mail.ru>
2022-02-22 16:16:47 +0300

    решено 10_store.py

---
+++ lesson_2/HW_2/10_store.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Есть словарь кодов товаров
+from pprint import pprint
+
+goods = {
+    'Лампа': '12345',
+    'Стол': '23456',
+    'Диван': '34567',
+    'Стул': '45678',
+}
+
+# Есть словарь списков количества товаров на складе.
+
+store = {
+    '12345': [
+        {'quantity': 27, 'price': 42},
+    ],
+    '23456': [
+        {'quantity': 22, 'price': 510},
+        {'quantity': 32, 'price': 520},
+    ],
+    '34567': [
+        {'quantity': 2, 'price': 1200},
+        {'quantity': 1, 'price': 1150},
+    ],
+    '45678': [
+        {'quantity': 50, 'price': 100},
+        {'quantity': 12, 'price': 95},
+        {'quantity': 43, 'price': 97},
+    ],
+}
+
+# Рассчитать на какую сумму лежит каждого товара на складе
+# например для ламп
+
+lamps_cost = store[goods['Лампа']][0]['quantity'] * store[goods['Лампа']][0]['price']
+# или проще (/сложнее ?)
+lamp_code = goods['Лампа']
+lamps_item = store[lamp_code][0]
+lamps_quantity = lamps_item['quantity']
+lamps_price = lamps_item['price']
+lamps_cost = lamps_quantity * lamps_price
+print('Лампа -', lamps_quantity, 'шт, стоимость', lamps_cost, 'руб')
+
+def quantity_cost():
+    for good in goods:
+        id = goods[good]
+        q = sum([x['quantity'] for x in store[str(id)]])
+        p = sum([x['price'] for x in store[str(id)]])
+        print(f'{good} - {q} шт, стоимость - {p} руб')
+
+quantity_cost()
+
+
+##########################################################################################
+# ВНИМАНИЕ! После того как __ВСЯ__ домашняя работа сделана и запушена на сервер,         #
+# нужно зайти в ЛМС (LMS - Learning Management System ) по адресу http://go.skillbox.ru  #
+# и оформить попытку сдачи ДЗ! Без этого ДЗ не будет проверяться!                        #
+# Как оформить попытку сдачи смотрите видео - https://youtu.be/qVpN0L-C3LU               #
+##########################################################################################
+
+
+
+
+
+
