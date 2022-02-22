304f8583c61f10b63dd779cc9d12a00762f9ffad
behappyman <behappyman@mail.ru>
2022-02-22 20:03:37 +0300

    решено 05_store.py

---
+++ lesson_3_основные_операторы/05_store.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+# Есть словарь кодов товаров
+
+goods = {
+    'Лампа': '12345',
+    'Стол': '23456',
+    'Диван': '34567',
+    'Стул': '45678',
+}
+
+# Есть словарь списков количества товаров на складе.
+
+store = {
+    '12345': [
+        {'quantity': 27, 'price': 42},
+    ],
+    '23456': [
+        {'quantity': 22, 'price': 510},
+        {'quantity': 32, 'price': 520},
+    ],
+    '34567': [
+        {'quantity': 2, 'price': 1200},
+        {'quantity': 1, 'price': 1150},
+    ],
+    '45678': [
+        {'quantity': 50, 'price': 100},
+        {'quantity': 12, 'price': 95},
+        {'quantity': 43, 'price': 97},
+    ],
+}
+
+# Рассчитать на какую сумму лежит каждого товара на складе.
+#
+def quantity_cost():
+    for good in goods:
+        id = goods[good]
+        q = sum([x['quantity'] for x in store[str(id)]])
+        p = sum([x['price'] for x in store[str(id)]])
+        print(f'{good} - {q} шт, стоимость - {p} руб')
+
+quantity_cost()
+
+
+
+
+
